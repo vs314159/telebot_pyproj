209eb908ff320dc1d7ac3e8b0d71e83416b59154
vitaliiholub <vitaliiholubit@gmail.com>
2023-01-19 21:03:18 +0200

    Виправлені синтаксичні помилки

--- pd_data.py
+++ pd_data.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 
 def remaining_lessons(name: str):
-<<<<<<< HEAD
-    """Повертає кулькість занять за іменем студента. Якщо його немає в базі - повертає NOne"""
+    """Повертає кількість занять за іменем студента. Якщо його немає в базі - повертає NOne"""
     name = ' '.join(word.capitalize() for word in name.split())
     df = pd.read_csv('Students_lessons.csv', sep=';')
 
@@ -16,25 +15,6 @@ def remaining_lessons(name: str):
 def write_to_cvs(name: str, study_format: str, amount_of_lessons: str, money: str) -> None:
     """Записує дані в файл 'Students_all_data.csv'"""
     name = ' '.join(word.capitalize() for word in name.split())
-=======
-    """
-    Повертає кулькість занять за іменем студента
-    """
-    df = pd.read_csv('Students_lessons.csv', sep=';')
-
-    name_ind = df.index[df["Ім'я"] == name].tolist()
-    ream_lessons = df["Кількість занять"].values[name_ind]
-    if ream_lessons.size > 0:
-        return ream_lessons[0]
-    else:
-        return None
-
-
-def write_to_cvs(name: str, study_format: str, amount_of_lessons: str, money: str) -> None:
-    """
-    Записує дані в файл 'Students_all_data.csv'
-    """
->>>>>>> 8d8ed83e2dacf299fc425e837015038f89cb16d1
     df = pd.DataFrame({
         "Ім'я": [name],
         "Формат навчання": [study_format],
@@ -42,7 +22,6 @@ def write_to_cvs(name: str, study_format: str, amount_of_lessons: str, money: st
         "Сумма": [money]
     })
     df.to_csv('Students_all_data.csv', mode='a', index=False, header=False, encoding='utf-8-sig', sep=';')
-<<<<<<< HEAD
 
 
 def is_student(name: str) -> bool:
@@ -50,5 +29,3 @@ def is_student(name: str) -> bool:
     name = ' '.join(word.capitalize() for word in name.split())
     df = pd.read_csv('Students_lessons.csv', sep=';')
     return (df["Ім'я"] == name).any()
-=======
->>>>>>> 8d8ed83e2dacf299fc425e837015038f89cb16d1
