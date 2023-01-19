915cbf44b80db5f67ceb7fb579ccff320a5b6a9d
vitaliiholub <vitaliiholubit@gmail.com>
2023-01-19 20:47:14 +0200

    Додано метод на перевірку чи є студент у базі

--- pd_data.py
+++ pd_data.py
@@ -2,23 +2,19 @@ import pandas as pd
 
 
 def remaining_lessons(name: str):
-    """
-    Повертає кулькість занять за іменем студента
-    """
+    """Повертає кулькість занять за іменем студента. Якщо його немає в базі - повертає NOne"""
+    name = ' '.join(word.capitalize() for word in name.split())
     df = pd.read_csv('Students_lessons.csv', sep=';')
 
     name_ind = df.index[df["Ім'я"] == name].tolist()
-    ream_lessons = df["Кількість занять"].values[name_ind]
-    if ream_lessons.size > 0:
-        return ream_lessons[0]
-    else:
-        return None
+    rem_lessons = df["Кількість занять"].values[name_ind]
+    if rem_lessons.size > 0:
+        return rem_lessons[0]
 
 
 def write_to_cvs(name: str, study_format: str, amount_of_lessons: str, money: str) -> None:
-    """
-    Записує дані в файл 'Students_all_data.csv'
-    """
+    """Записує дані в файл 'Students_all_data.csv'"""
+    name = ' '.join(word.capitalize() for word in name.split())
     df = pd.DataFrame({
         "Ім'я": [name],
         "Формат навчання": [study_format],
@@ -26,3 +22,10 @@ def write_to_cvs(name: str, study_format: str, amount_of_lessons: str, money: st
         "Сумма": [money]
     })
     df.to_csv('Students_all_data.csv', mode='a', index=False, header=False, encoding='utf-8-sig', sep=';')
+
+
+def is_student(name: str) -> bool:
+    """Перевірка наявності студента у базі данних"""
+    name = ' '.join(word.capitalize() for word in name.split())
+    df = pd.read_csv('Students_lessons.csv', sep=';')
+    return (df["Ім'я"] == name).any()
