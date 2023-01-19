18d534e7436469996f66e2ed779f8b279dcc8703
vitaliiholub <vitaliiholubit@gmail.com>
2023-01-19 23:01:43 +0200

    Додано метод apply_for_trial_lesson

--- pd_data.py
+++ pd_data.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
 
+def is_student(name: str) -> bool:
+    """Перевірка наявності студента у базі данних"""
+    name = ' '.join(word.capitalize() for word in name.split())
+    df = pd.read_csv('Students_lessons.csv', sep=';')
+    return (df["Ім'я"] == name).any()
+
+
 def remaining_lessons(name: str):
     """Повертає кількість занять за іменем студента. Якщо його немає в базі - повертає NOne"""
     name = ' '.join(word.capitalize() for word in name.split())
@@ -12,8 +19,8 @@ def remaining_lessons(name: str):
         return rem_lessons[0]
 
 
-def write_to_cvs(name: str, study_format: str, amount_of_lessons: str, money: str) -> None:
-    """Записує дані в файл 'Students_all_data.csv'"""
+def write_student_data(name: str, study_format: str, amount_of_lessons: str, money: str) -> None:
+    """Записує дані студента в файл 'Students_all_data.csv'"""
     name = ' '.join(word.capitalize() for word in name.split())
     df = pd.DataFrame({
         "Ім'я": [name],
@@ -24,8 +31,21 @@ def write_to_cvs(name: str, study_format: str, amount_of_lessons: str, money: st
     df.to_csv('Students_all_data.csv', mode='a', index=False, header=False, encoding='utf-8-sig', sep=';')
 
 
-def is_student(name: str) -> bool:
-    """Перевірка наявності студента у базі данних"""
+def apply_for_trial_lesson(name: str, study_format: str, phone_number: str, suitable_time: str, test_res: str) -> None:
+    """Записує дані студента, бажаючого потрапити на пробне заняття в файл 'Trial_lessons.csv'"""
     name = ' '.join(word.capitalize() for word in name.split())
-    df = pd.read_csv('Students_lessons.csv', sep=';')
-    return (df["Ім'я"] == name).any()
+    df = pd.DataFrame({
+        "Ім'я": [name],
+        "Формат навчання": [study_format],
+        "Номер Телефону": [phone_number],
+        "Зручний час": [suitable_time],
+        "Результат тестування": [test_res]
+    })
+    df.to_csv('Trial_lessons.csv', mode='a', index=False, header=False, encoding='utf-8-sig', sep=';')
+
+
+apply_for_trial_lesson(name='Анатолій Боднаренко',
+                       study_format='індивідуально',
+                       phone_number='987654321?',
+                       suitable_time='Будь який час на вихідних',
+                       test_res='B2')
